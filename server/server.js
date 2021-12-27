394dbd6cf86b27800785a19fc1022433862e9b66
ulisesten <ulises.mtz.el@gmail.com>
2021-12-26 21:17:54 -0600

    added cors for online test server

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ server.register(PublicationsController)*/
 
 
 server.register(fastifyCors, {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://the-code-react.herokuapp.com'],
     optionsSuccessStatus: 200 
 })
 
